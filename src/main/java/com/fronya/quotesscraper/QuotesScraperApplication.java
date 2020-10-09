63bfa7bfc4f60a25803018d687c78a835e1eed3d
Volha <gubareva_olga@outlook.com>
2020-10-10 00:22:08 +0300

    Init commit

---
+++ src/main/java/com/fronya/quotesscraper/QuotesScraperApplication.java
@@ -0,0 +1,13 @@
+package com.fronya.quotesscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesScraperApplication.class, args);
+	}
+
+}
